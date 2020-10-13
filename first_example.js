8812a08d3c847a12a332c3cd640532f164484a2b
Nikhil Mahapatra <64895246+Nikhil-Mahapatra@users.noreply.github.com>
2020-10-13 13:01:04 +0530

    Day-1
    Day-1

---
+++ first_example.js
@@ -0,0 +1,3 @@
+console.log("Hello! Node");
+console.log(__dirname);
+console.log(__filename);
---
+++ http.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require("fs");
+var url = require('url');
+
+http.createServer(function (req, res){
+      var path = url.parse(req.url).path;
+
+      console.log("Request for " + path + " received");
+
+      fs.readFile(path.substr(1), function(err, data){
+            if(err){
+                  console.log(err);
+                  res.writeHead(404, {'Content-Type': 'text/html'});
+                  res.write('Error Page not found!');
+            }else{
+                  res.writeHead(200,{'Content-Type': 'text/html'})
+                  res.write(data.toString());
+            }
+            res.end();
+      });
+}).listen(5000);
+
+console.log('Server running at localhost:3000');
